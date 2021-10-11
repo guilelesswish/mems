fa41b7470fbdc323e70a8468d230ce19ff30307e
guilelesswish <92335477+guilelesswish@users.noreply.github.com>
2021-10-11 22:54:32 +0300

    V0.1.1 Alpha
    V0.1.1 Alpha

---
+++ config.py
@@ -0,0 +1,5 @@
+settings = {
+    'CLIENT_ID':'ngQduNsktGHnNdHG0f_nlg',
+    'SECRET_CODE':'eY8BnwA85Jst2iWBqvxWfYCypvb9Sg',
+    'TOKEN':'16a09da33af1805b130b365a70dca1128dd9e0334d8615311ba962c7e07d2f9a2285149fb60f0beeb4ac9'
+}
